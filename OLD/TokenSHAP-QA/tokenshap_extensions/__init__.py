81a6827298b41fc6666d1bd2cd1936cb0a7cd436
junjieSJSU <junjie.chong@sjsu.edu>
2026-05-07 14:56:38 -0700

    removed deprecated items

--- OLD/TokenSHAP-QA/tokenshap_extensions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .qa_tokenshap import QATokenSHAP
-from .extractors import qa_extractor
-
-__all__ = ["QATokenSHAP", "qa_extractor"]
--- OLD/TokenSHAP-QA/tokenshap_extensions/value_functions/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Value function extensions for TokenSHAP.
-Includes placeholders and experimental scoring methods.
-"""
-
-from .correctness_value import CorrectnessValueFunction
-
-__all__ = ["CorrectnessValueFunction"]
